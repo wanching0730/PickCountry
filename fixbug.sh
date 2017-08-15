f071674e509f35e78f667613fc42246cb2eb52b7
Wan Ching <wanching_97@hotmail.com>
2017-08-16 00:35:48 +0800

    edited

--- fixbug.sh
+++ fixbug.sh
@@ -11,3 +11,5 @@ sdkmanager "extras;google;m2repository"
 # For older Android SDK:
 echo y | android update sdk --no-ui --all --filter extra-android-m2repository | grep 'package installed'
 echo y | android update sdk --no-ui --all --filter extra-google-m2repository | grep 'package installed'
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2"
